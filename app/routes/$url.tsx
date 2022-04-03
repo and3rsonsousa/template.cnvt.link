a713882c6761300b6cb864c9763e8de78ef48515
Anderson Sousa <and3rsonsousa@outlook.com>
2022-04-03 15:53:16 -0300

    links in groups

--- app/routes/$url.tsx
+++ app/routes/$url.tsx
@@ -1,12 +1,29 @@
 import { LoaderFunction, redirect } from "remix";
 import { getAppData } from "~/lib/app.server";
-import { AppDataType } from "~/types";
+import { AppDataType, ItemType } from "~/types";
 
 export const loader: LoaderFunction = async ({ params }) => {
 	let data: AppDataType = await getAppData();
-	let link = data.links.filter((link) => link.url === params.url)[0];
+	let itemLink: ItemType | undefined;
 
-	if (!link) throw new Error("Não existe Link");
-	if (!link.redirect) throw new Error("Não existe Redirect");
-	return redirect(link.redirect);
+	data.links.map((link) => {
+		if (link.group) {
+			link.group.forEach((link_group) => {
+				if (link_group.url === params.url) {
+					itemLink = link_group;
+				}
+			});
+		} else {
+			if (link.url === params.url) {
+				itemLink = link;
+			}
+		}
+		return false;
+	});
+
+	console.log(itemLink);
+
+	if (!itemLink) throw new Error("Não existe Link");
+	if (!itemLink.redirect) throw new Error("Não existe Redirect");
+	return redirect(itemLink.redirect);
 };
